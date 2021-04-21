e1646a1a3cc634789a862dc9fd897ded4f3f1cef
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-04-22 02:20:04 +0530

    Add files via upload
    Printing Product Tables and Pyramid problems through looping

---
+++ All about Looping in python/problem3A1CO.py
@@ -0,0 +1,10 @@
+'''
+Program :- create a variable , assign it to any value, and print 1 to n
+
+Author :- AmRiyaz
+
+Last Modified :- April 2021
+'''
+n = 5
+for i in range(1,n+1):
+    print(i)
---
+++ All about Looping in python/problem4A2CO.py
@@ -0,0 +1,12 @@
+'''
+Program :- Print sum of cubes from number 1 to n
+
+Author :- AmRiyaz
+
+Last Modified :- April 2021
+'''
+n = 3
+a = 0
+for i in range(1,n+1):
+    a = a + i*i*i
+print(a)
