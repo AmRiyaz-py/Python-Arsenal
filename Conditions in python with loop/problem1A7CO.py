645f36f78a5b44effb9d6f6af48a4427772cc562
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-04-26 01:15:57 +0530

    python code
    this print the n with n number of times

---
+++ Conditions in python with loop/problem1A7CO.py
@@ -0,0 +1,19 @@
+'''
+Program : Given a digit n as input print digit n , n number of times
+
+Author : AmRiyaz
+
+Last Modified : April 2021
+'''
+n = 9
+num = n
+st= ""
+
+for i in range(1,n+1):
+    st = st + str(num)
+print(st)
+
+'''
+Output:
+999999999
+'''
