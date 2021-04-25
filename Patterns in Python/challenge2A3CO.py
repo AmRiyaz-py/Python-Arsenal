5d432cda6fef358b32406ba266865e1df56a00b6
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-04-26 02:06:26 +0530

    half pyramid pattern
    printing the half pyramid pattern till the n number.

---
+++ Patterns in Python/challenge2A3CO.py
@@ -0,0 +1,19 @@
+'''
+Program :- Given n as input,print following pattern using for loop
+
+Author : AmRiyaz
+
+Last Modified : April 2021
+'''
+n = 3
+st = ""
+for i in range(1, n+1):
+    st = st + str(i)
+    print(st)
+
+'''
+Output:-
+1
+12
+123
+'''
