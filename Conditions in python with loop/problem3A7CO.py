53108c68c7edeacac08c0442f6dc9958858a9058
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-04-26 01:28:04 +0530

    python program for two to the power value
    this program will print the value of 2 ^ n , what ever the n is being given in the program it will work accordingly.

---
+++ Conditions in python with loop/problem3A7CO.py
@@ -0,0 +1,32 @@
+'''
+Program : Given n as input , print value of 2^n
+
+Author : AmRiyaz
+
+Last Modified : April 2021
+'''
+
+
+n = 4
+a = 2
+st = 1
+
+for i in range(1,n+1):
+    st = a*st
+print(st)
+
+'''
+Output : 
+16
+'''
+
+
+
+
+
+
+
+
+
+
+
