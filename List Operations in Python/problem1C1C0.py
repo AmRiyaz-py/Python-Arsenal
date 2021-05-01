8d9afa965dbfd251d9a1f2a02841f42ae4539829
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-05-02 01:18:16 +0530

    Operations in the lIst
    suming up all the elements inside a list.

---
+++ List Operations in Python/problem1C1C0.py
@@ -0,0 +1,20 @@
+'''
+Program :-Given a list as input. Print sum of all elements.
+
+Author :- AmRiyaz
+
+Last Modified :- April 2021
+'''
+
+st = [1,2,3,4]
+len1 = len(st)
+sum = 0
+for i in range(0,len1):
+    ch = st[i]
+    sum = sum + ch
+print(sum)
+
+'''
+Output:
+10
+'''
