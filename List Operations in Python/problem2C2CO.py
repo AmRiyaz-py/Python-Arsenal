c1cdc957ab4c6df7cbd183e01add23458f35e8ad
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-05-03 23:54:58 +0530

    List Operation
    Increment all the element of given list by 1

---
+++ List Operations in Python/problem2C2CO.py
@@ -0,0 +1,21 @@
+'''
+Program :-Given a List as input. Increment all elements in the given List by 1
+
+Author :- AmRiyaz
+
+Last Modified :- April 2021
+'''
+st = [1,2,12,5]
+len1 =len(st)
+for i in range(0,len1):
+    ch = st[i]
+    a = ch+1
+    print(a)
+
+'''
+Output:
+2
+3
+13
+6
+'''
