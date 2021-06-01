690935471ad15e94c73debb1144473c0c86a05be
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-06-01 13:19:47 -0700

    Hash Pattern
    Given n as input. Print following pattern using For loop.

---
+++ Patterns in Python/task3PR1A.py
@@ -0,0 +1,27 @@
+'''
+Program:- Given n as input. Print following pattern using For loop.
+#
+##
+###
+####
+#####
+
+Author:- AmRiyaz
+
+Last Modified:- May 2021
+'''
+n = 5
+hash = "#" 
+a= ""
+for i in range(1,n+1):
+    a = a + hash
+    print(a)
+
+'''
+Output:
+#
+##
+###
+####
+#####
+'''
