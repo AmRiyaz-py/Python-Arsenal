3ef0fa56897ae293f9e091492751a6067c8285fb
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-06-01 13:18:44 -0700

    For loop in number system.
    Given n as input. Print product of all numbers from 1 to n.

---
+++ All about Looping in python/task2PR1A.py
@@ -0,0 +1,17 @@
+'''
+Program:- Given n as input. Print product of all numbers from 1 to n.
+
+Author:- AmRiyaz
+
+Last Modified:- May 2021
+'''
+n = 5
+a = 1
+for i in range(1,n+1):
+    a = i * a
+print(a) 
+
+'''
+Output:
+120
+'''
